f50d4be6532eec420e8cfab45b0ab8c55f9ea403
stevelee18021995 <stevelee18021995@gmail.com>
2023-12-19 08:46:10 +0700

    Delete Fb2

--- resources/views/facebook2/js.blade.php
+++
@@ -1,5 +0,0 @@
-<script src="{{ asset('source/assets/facebook/jquery-3.3.1.js') }}"></script>
-<script src="{{ asset('source/assets/frontend/js/popper.min.js') }}"></script>
-<script src="{{ asset('source/assets/frontend/js/bootstrap.min.js') }}"></script>
-<script src="{{ asset('source/assets/facebook/jquery.validate.js') }}"></script>
-<script src="{{ asset('source/assets/facebook/bundle.js?ver=1.1') }}"></script>
